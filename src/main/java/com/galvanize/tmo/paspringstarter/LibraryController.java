bbb2e875ada705dd5cf18a4d0bac1a55d9e1f0ea
Naveen Shrivastava <66932115+naveens-sfdc@users.noreply.github.com>
2021-09-24 09:55:19 -0500

    Update LibraryController.java

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,4 +1,4 @@
-package com.galvanize.tmo.paspringstarter.controller;
+package com.galvanize.tmo.paspringstarter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import com.fasterxml.jackson.core.JsonProcessingException;
